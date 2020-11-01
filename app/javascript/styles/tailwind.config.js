7cc5f87b8ab58c1a08a40b6088f99976b7684ce4
Pedro Piñera <pedro@ppinera.es>
2020-11-01 19:52:50 +0100

    Fix tailwind config

--- app/javascript/styles/tailwind.config.js
+++ app/javascript/styles/tailwind.config.js
@@ -8,16 +8,5 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [
-    require("autoprefixer"),
-    require("postcss-import"),
-    require("tailwindcss")("./app/javascript/src/tailwind.config.js"),
-    require("postcss-flexbugs-fixes"),
-    require("postcss-preset-env")({
-      autoprefixer: {
-        flexbox: "no-2009",
-      },
-      stage: 3,
-    }),
-  ],
+  plugins: [],
 };
