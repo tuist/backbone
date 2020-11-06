0b2188eee1c300cd0ff75a3af6e5f150f9535644
Pedro Piñera <pepibumur@gmail.com>
2020-11-06 12:43:53 +0100

    Fix url

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -17,7 +17,7 @@ environment.plugins.append(
     API_URL:
       process.env.NODE_ENV === "development"
         ? JSON.stringify("http://127.0.0.1:3000")
-        : JSON.stringify("https://app.tuistpro.io"),
+        : JSON.stringify("https://stats.tuist.io"),
     SENTRY_FRONTEND_DSN: process.env.SENTRY_FRONTEND_DSN
       ? JSON.stringify(process.env.SENTRY_FRONTEND_DSN)
       : JSON.stringify(""),
