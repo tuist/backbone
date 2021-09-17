b623a75321b97d33a21d92b88ba61e15e641df39
Pedro Piñera <pepibumur@gmail.com>
2021-09-17 17:30:51 +0200

    Remove some unnecessary dependencies

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  verbose: true,
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["<rootDir>/app/javascript/**/*.test.(ts|tsx)"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
-};
